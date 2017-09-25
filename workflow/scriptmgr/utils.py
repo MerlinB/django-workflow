3b2dc5a555e8b5318a7889d3ba95d787e1fd0736
MerlinB <merlin.buczek@gmail.com>
2017-09-25 23:29:50 +0200

    work copy

---
+++ workflow/scriptmgr/utils.py
@@ -0,0 +1,6 @@
+def test1():
+    pass
+
+def test2(bla, test='Test'):
+    '''A print function'''
+    print(bla, test)
--- workflow/scriptmgr/views.py
+++ workflow/scriptmgr/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from django.contrib.admin.views.decorators import staff_member_required
+from .forms import Rechnung2PdfForm
+import inspect
+from . import utils
 
-# Create your views here.
+
+skripte_dir = "/home/scholarium/Skripte/"
+python_bin = skripte_dir+"venv/bin/python3.6"
+
+
+@staff_member_required
+def control_view(request):
+    functions = inspect.getmembers(utils, inspect.isfunction)
+    skripte = {}
+    context = {
+        'skripte': functions,
+    }
+    for f in functions:
+        print(str(inspect.signature(utils.f)))
+    return render(request, 'workflow/skripte-view.html', context)
+
+def rechnung_view(request):
+    if request.method == 'POST':
+        subprocess.Popen([python_bin, skripte_dir+'Text/Rechnung2Pdf.py'])
+
+    form = Rechnung2PdfForm
+
+    # skripte = {
+    #     'Rechnung2Pdf': '/home/scholarium/Skripte/Text/Rechnung2Pdf'
+    # }
+    context = {
+        'form': form
+    }
+    return render(request, 'workflow/form-view.html', context)
